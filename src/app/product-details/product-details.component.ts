b63857acfe2d2c5b80ce9a83a1afd2a4748f6a9a
moshiurmurshed <66069698+moshiurmurshed@users.noreply.github.com>
2022-12-01 14:49:09 +0100

    Navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent {
-
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const prodIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find((p) => p.id == prodIdFromRoute);
+  }
 }
