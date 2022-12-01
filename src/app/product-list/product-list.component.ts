355f299ad37206e09e1235b24de7374534c6484c
moshiurmurshed <66069698+moshiurmurshed@users.noreply.github.com>
2022-12-01 13:27:10 +0100

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,13 +8,13 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
+  products = [...products];
 
   share() {
     window.alert('The product has been shared!');
   }
   onNotify() {
-    window.alert('You will be notified when the product goes on sale');
+    window.alert('You will be notified about this product');
   }
 }
 
